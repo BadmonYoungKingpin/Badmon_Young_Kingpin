b0bc677e768f9fab0bb8c488ecbd65c309968b97
BadmonYoungKingpin <u13080432@tuks.co.za>
2015-10-07 08:47:52 +0200

    ListView working. (07/10)

--- app/src/main/java/com/example/hp/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hp/myapplication/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.hp.myapplication;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.homepage);
     }
 
+    /********************************  For the BACKButtons ****************************************
+     *
+     * @description
+     *
+     *********************************************************************************************/
     public void backHomeButtonClick(View v) {
         setContentView(R.layout.homepage);
     }
@@ -29,8 +42,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
     }
 
+    /********************************  For the Main Menu ****************************************
+     *
+     * @description
+     *
+     *********************************************************************************************/
     public void eventsButtonClick(View v) {
         setContentView(R.layout.event_home);
+        Intent intent = new Intent(this, eventListPopActivity.class);
+
+        /*ListView editText = (ListView) findViewById(android.R.id.list);
+        String[] list1 = {"asdasdasd","addddaadad","hhfghfsdsa"};*/
+
+        //Send data along to retrieve and use with new activity
+        String list1 = "asdasdasd";
+        intent.putExtra(EXTRA_MESSAGE, list1);
+        //Start the new activity
+        startActivity(intent);
     }
     public void todoButtonClick(View v) {
         setContentView(R.layout.activity_home_page);
@@ -39,11 +67,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.weather_home);
     }
 
+    /********************************  For the EVENTSList ****************************************
+     *
+     * @description xczc
+     *
+     *********************************************************************************************/
     public void addeventButtonClick(View v) {
         setContentView(R.layout.event_new);
     }
 
+    public void saveeventButtonClick(View v) {
 
+    }
+
+    /********************************  For the WEATHERDisplay *************************************
+     *
+     * @description
+     *
+     *********************************************************************************************/
+
+    /********************************  For the TODOList *******************************************
+     *
+     * @description
+     *
+     *********************************************************************************************/
     public void workButtonClick(View v) {
         setContentView(R.layout.work_page);
     }
@@ -57,6 +104,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.other_page);
     }
 
+
+    /********************************  DEFAULT STUFF **********************************************
+     *
+     * @description
+     *
+     *********************************************************************************************/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
